6b8b421e1305e1a7841eac387a6134ab0982b2f8
Jining Liu <jiningdev@gmail.com>
2025-03-10 16:27:18 -0500

    init, word index

---
+++ template.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+let start = DispatchTime.now().uptimeNanoseconds
+
+print("================================")
+print("Dalloway Project")
+print("Program Template")
+print("================================")
+
+do {
+
+    let file = "/Users/jining/Library/Mobile Documents/com~apple~CloudDocs/School/11/Lit/Dalloway Project/text.txt"
+    let path = URL(fileURLWithPath: file)
+    var text = try String(contentsOf: path, encoding: .utf8)
+
+//    text = text.replacingOccurrences(of: "?", with: "")
+//    text = text.replacingOccurrences(of: ".", with: "")
+//    text = text.replacingOccurrences(of: ",", with: "")
+//    text = text.replacingOccurrences(of: "!", with: "")
+//    text = text.replacingOccurrences(of: "—", with: " ")
+//    text = text.replacingOccurrences(of: "“", with: "")
+//    text = text.replacingOccurrences(of: "”", with: "")
+//    text = text.replacingOccurrences(of: "(", with: "")
+//    text = text.replacingOccurrences(of: ")", with: "")
+//    text = text.replacingOccurrences(of: ";", with: "")
+
+    print(text)
+
+} catch {
+    print("Error: \(error.localizedDescription)")
+}
+
+print("================================")
+print("Done ✅")
+print("Execution Time: \((DispatchTime.now().uptimeNanoseconds - start) / 1000000)ms")
